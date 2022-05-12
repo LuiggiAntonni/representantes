6dd80fb71d081d38734fb9a51ea714352ac34e83
LuiggiAntonni <luiggiantonni0129@gmail.com>
2022-05-12 16:05:53 -0300

    Criando database

--- index.js
+++ index.js
@@ -17,7 +17,28 @@ var con = mysql.createConnection({
 con.connect(function (err) {
     if (err) throw err;
     console.log("Conectou ao bano de dados");
-});
+
+    con.query("CREATE DATABASE IF NOT EXISTS representante", function (err, result) {
+        if (err) throw err;
+        console.log("Database iniciada");
+    });
+
+    con.changeUser({database : 'representante'}, function(err) {
+        if (err) throw err;
+      });
+
+    var sql = "CREATE TABLE IF NOT EXISTS catalogo (CatalogoID INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL, ano VARCHAR(255) NOT NULL, imagem VARCHAR(255) DEFAULT \'semimagem.png\')";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Tabela Catalogos iniciada");
+    });
+
+    var sql2 = "CREATE TABLE IF NOT EXISTS produtos (id INT AUTO_INCREMENT PRIMARY KEY, ref VARCHAR(255) NOT NULL, imagem VARCHAR(255) DEFAULT \'semimagem.png\', tamanhos VARCHAR(10) NOT NULL, cores VARCHAR(255) NOT NULL, catalogo_FK INT, FOREIGN KEY (catalogo_FK) REFERENCES catalogo (CatalogoID))";
+    con.query(sql2, function (err, result) {
+        if (err) throw err;
+        console.log("Tabela Produtos iniciada");
+    });
+}); 
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
